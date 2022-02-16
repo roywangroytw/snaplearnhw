d798da8326b9b9a55c67aa77ce1f1b5091b2d7d2
roywangroytw <roywangroy.tw@gmail.com>
2022-02-16 14:18:20 +0800

    Added 404 logic if encountering record not found, and authenticate user for purchase page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   include UsersHelper
   include CoursesHelper
 
+  rescue_from ActiveRecord::RecordNotFound, with: :result_not_found
+
   private
+
+  def result_not_found
+    render file: 'public/404.html', layout: false, status: 404
+  end
 
   def authenticate_admin
     if not user_is_login?
